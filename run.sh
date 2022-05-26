dec98cc184df52507dc8fff6456dc67f0cbd4530
Parsa Noori <parsa@Parsas-MacBook-Pro.local>
2022-05-26 18:24:05 +0430

    change rsa.fdl and run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+clear
+
 echo "--> compiling sw"
 arm-linux-gcc -static -Wall -O3 rsa.c -o rsa
 
 
 echo "--> run gplatform"
-gplatform rsa.fdl
+gplatform rsa.fdl 2>/dev/null
 
 echo "--> done"
