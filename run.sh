c535b34477643994345c1891fd3ac05f0c5f2661
mostafa <mos1380n@gmail.com>
2022-05-26 17:28:08 +0430

    Cleanup

--- run.sh
+++ run.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-echo "--> compiling sw"
+echo "--> compiling sw..."
 arm-linux-gcc -static -Wall -O3 rsa.c -o rsa
-
-
-echo "--> run gplatform"
+echo "--> run gplatform..."
 gplatform rsa.fdl
-
 echo "--> done"
